2ef29e16aeb6bf817c9499f7b2e4c340b9c65227
Barbara Liau <bj.liau@gmail.com>
2015-01-09 11:54:52 -0700

    Completed more JS problems

--- practice.js
+++ practice.js
@@ -17,8 +17,6 @@ var name = 'Tyler';
 
 //Next problem
 
-
-
 //Create a function called getName that uses prompt() to prompt the user for their name, then returns the name.
 
 
@@ -29,8 +27,6 @@ var name = 'Tyler';
 
 
 //Next Problem
-
-
 
 //Create a function called welcome that uses your getName function you created in the previous problem to get the users name,
 //then alerts "Welcome, " plus whatever the users name is.
@@ -43,13 +39,14 @@ var name = 'Tyler';
 //Next problem
 
 
-
 //Write a function called adder that uses the 'arguments' keyword that comes with every function to
 //loop over all the arguments the function received, add them all together, then return the result.
 //Save the result of the return value from your adder function, then alert "The Total Number was " + the number that was
 //returned from adder.
 
-  //Code Here
+  var adder = function() {
+
+  };
 
 
 //Next Problem
@@ -58,7 +55,9 @@ var name = 'Tyler';
 
 //What is the difference between arguments and parameters?
 
-  //Answer Here
+  Arguments are things you pass when you call a function; 
+  parameters are placeholders for where your argument value is going to go
+
 
 
 //Next problem
@@ -68,7 +67,8 @@ var name = 'Tyler';
 //What are all the falsy values in JavaScript and how do you check if something is falsy?
 
 
-  //Answer Here
+  0, NaN, undefined, false, null, ""
+ You can use !== or !variable
 
 
 
@@ -78,15 +78,17 @@ var name = 'Tyler';
 
 //Create a function called myName that returns your name
 
-  //Code Here
+  var myName = function() {
+    return "Barbara";
+  };
 
 //Now save the function definition of myName into a new variable called newMyName
 
-  //Code Here
+ var newMyName = myName;
 
 //Now alert the result of invoking newMyName
 
-
+alert(newMyName());
 
 //Next problem
 
@@ -94,10 +96,17 @@ var name = 'Tyler';
 
 //Create a function called outerFn which returns an anonymous function which returns your name.
 
-  //Code Here
+  var outerFn = function () {
+    return function () {
+      return "Barbara";
+    };
+
+  };
 
 //Now save the result of invoking outerFn into a variable called innerFn.
 
-  //Code Here
+  var innerFn = outerFn();
 
 //Now invoke innerFn.
+
+ innerFn();
